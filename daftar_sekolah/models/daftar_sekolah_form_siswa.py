74d752c8fbcc45228c3604d1d856d6e16b7f842b
M Raehan Akbar <43035458+mraehanakbar@users.noreply.github.com>
2023-02-27 21:01:01 +0700

    new training
    new training

--- daftar_sekolah/models/daftar_sekolah_form_siswa.py
+++ daftar_sekolah/models/daftar_sekolah_form_siswa.py
@@ -15,7 +15,19 @@ class DaftarSekolahFormSiswa(models.Model):
         ('ragil','Ragil Jerman'),
     ],string="Jenis Kelamin")
 
+
+    def get_data_email_login(self):
+        try:
+            search_data = self.env['employee.data'].search([('email_employee', '=', self.env.user.login)])
+            position = search_data.mapped('organization_employee')[0]
+            return position
+        except:
+            position = ''
+            return position
+
     related_fields = fields.Many2one('employee.data', string='Superior',index=True)
+    login_users = fields.Many2one('res.users', string='Name', index=True, tracking =2, default=lambda self: self.env.user) 
+    departement_employee = fields.Char(string="departement employee", default= get_data_email_login, readonly=True)
     email_data_employee = fields.Char(related='related_fields.email_employee')
     employee_organization = fields.Selection(related='related_fields.organization_employee')
     current_organization = fields.Char(string="curr organization")
